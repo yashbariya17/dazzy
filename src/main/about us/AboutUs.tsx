febd0ac80a8a1226df756c1c3d396a22f640f249
Pathik Chauhan <pathik.c@ciright.com>
2025-06-05 18:17:44 +0530

    added some text in about us page

--- src/main/about us/AboutUs.tsx
+++ src/main/about us/AboutUs.tsx
@@ -4,6 +4,9 @@ const AboutUs = () => {
     <section className="bg-gray-500 text-5xl text-white text-center py-16">
         <h2>About Us</h2>
     </section>
+    <section className="bg-amber-950 text-[4.5rem] text-center py-4">
+        <h2 className="text-transparent bg-clip-text bg-gradient-to-b from-yellow-600 via-yellow-200 to-yellow-600 from-20% via-45%  to-100%" >25 YEARS OF SWEET MEMORIES</h2>
+    </section>
   </main>;
 };
 
