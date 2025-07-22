0454bf492f95e05a8d009a32d32b04ca6b2f3df2
yash <yash.b@ciright.com>
2025-07-22 22:01:26 +0530

    added images

--- src/main/Products/Products.tsx
+++ src/main/Products/Products.tsx
@@ -89,13 +89,426 @@ const productObj: productType = {
     // { "name": "Celio Crunchy", "url": "/images/centerfilledchocolate/DECORATIVE-CHOCOLATE.png" },
   ],
   "decorative chocolate": [],
-  "peanut chocolate": [],
-  "nought bar": [],
-  "wafer rolls": [],
-  toffee: [],
-  jelly: [],
-  candy: [],
-  lollipop: [],
+  "peanut chocolate": [{
+    name: "00126",
+    url: "/images/peanut chocolate/00126.png",
+  },
+  {
+    name: "Caranut Box",
+    url: "/images/peanut chocolate/CARANUT BOX.png",
+  },
+  {
+    name: "Chokers Box",
+    url: "/images/peanut chocolate/CHOKERS BOX.png",
+  },
+  {
+    name: "Chokers Container",
+    url: "/images/peanut chocolate/CHOKERS CONTAINER.png",
+  },
+  {
+    name: "Oh Wow Big Box",
+    url: "/images/peanut chocolate/OH WOW BIG BOX.png",
+  },
+  {
+    name: "Oh Wow Pouch",
+    url: "/images/peanut chocolate/OH WOW POUCH.png",
+  },
+  {
+    name: "Oh Wow Small Box",
+    url: "/images/peanut chocolate/OH WOW SMALL BOX.png",
+  },
+  {
+    name: "Oh Wow Standy Pouch",
+    url: "/images/peanut chocolate/OH WOW STANDY POUCH.png",
+  }],
+  "nought bar": [{
+    name: "BIG STAR",
+    url: "/images/nugut bar/BIG STAR.png",
+  },
+  {
+    name: "TORNUTTE  BAR CONTAINER",
+    url: "/images/nugut bar/TORNUTTE  BAR CONTAINER.png",
+  },
+{
+    name: "BRACKERS BOX",
+    url: "/images/nugut bar/BRACKERS BOX.png",
+  },
+{
+    name: "TORNUTTE  BAR BOX",
+    url: "/images/nugut bar/TORNUTTE  BAR BOX.png",
+  },
+],
+
+  "wafer rolls": [{
+    name: "3 Stix Chocofills",
+    url: "/images/wafffer rolls/3 stix chocofills.png",
+  },
+  {
+    name: "3 Stix Kraks",
+    url: "/images/wafffer rolls/3 stix kraks.png",
+  },
+  {
+    name: "3stix Milk Fills",
+    url: "/images/wafffer rolls/3stix milk fills.png",
+  },
+  {
+    name: "Big Roll Chocolate",
+    url: "/images/wafffer rolls/big roll chocolate.png",
+  },
+  {
+    name: "Big Roll Dark",
+    url: "/images/wafffer rolls/big roll dark.png",
+  },
+  {
+    name: "Big Roll Milk",
+    url: "/images/wafffer rolls/big roll milk.png",
+  },
+  {
+    name: "Choco Stix",
+    url: "/images/wafffer rolls/choco stix.png",
+  },
+  {
+    name: "Dark Stix",
+    url: "/images/wafffer rolls/dark stix.png",
+  },
+  {
+    name: "Krack Stix",
+    url: "/images/wafffer rolls/krack stix.png",
+  },
+  {
+    name: "Rolle Stix",
+    url: "/images/wafffer rolls/rolle stix.png",
+  },
+  {
+    name: "Wafeer Roll",
+    url: "/images/wafffer rolls/wafeer roll.png",
+  }],
+  toffee: [{
+    name: "Almond Zupadi Box",
+    url: "/images/toffee/ALMOND ZUPADI  BOX.png",
+  },
+  {
+    name: "Anjeer Zupadi Box",
+    url: "/images/toffee/ANJEER ZUPADI BOX.png",
+  },
+  {
+    name: "Butter Feast Zupadi Box",
+    url: "/images/toffee/BUTTER FEAST ZUPADI BOX.png",
+  },
+  {
+    name: "Butter Scotch Zupadi Box",
+    url: "/images/toffee/BUTTER SCOTCH ZUPADI BOX.png",
+  },
+  {
+    name: "Caramal Honey",
+    url: "/images/toffee/CARAMAL HONEY.png",
+  },
+  {
+    name: "Choco Almond Grand",
+    url: "/images/toffee/CHOCO ALMOND GRAND.png",
+  },
+  {
+    name: "Cocolile Zupadi Box",
+    url: "/images/toffee/COCOILLE ZUPADI BOX.png",
+  },
+  {
+    name: "Coconut Butter Zupadi Box",
+    url: "/images/toffee/COCONUT BUTTER ZUPADI BOX.png",
+  },
+  {
+    name: "Dairy Way Zupadi Box",
+    url: "/images/toffee/DAIRY WAY ZUPADI BOX.png",
+  },
+  {
+    name: "Dry Fruit Selection Gift Box",
+    url: "/images/toffee/dry fruit selection gift box.png",
+  },
+  {
+    name: "Dry Fruit Selection Gift Pouch",
+    url: "/images/toffee/dry fruit selection gift pouch.png",
+  },
+  {
+    name: "Eclair Anjeer Cream Gift Box",
+    url: "/images/toffee/eclair anjeer cream gift box.png",
+  },
+  {
+    name: "Eclair Anjeer Cream",
+    url: "/images/toffee/eclair anjeer cream.png",
+  },
+  {
+    name: "Eclair Coconut Cream Gift Box",
+    url: "/images/toffee/eclair coconut cream gift box.png",
+  },
+  {
+    name: "Eclair Coconut Cream",
+    url: "/images/toffee/eclair coconut cream.png",
+  },
+  {
+    name: "Eclair Dark Cream Gift Box",
+    url: "/images/toffee/eclair dark cream gift box.png",
+  },
+  {
+    name: "Eclair Dark Cream",
+    url: "/images/toffee/eclair dark cream.png",
+  },
+  {
+    name: "Eclair Dark Jar",
+    url: "/images/toffee/eclair dark jar.png",
+  },
+  {
+    name: "Eclair Dark Pouch",
+    url: "/images/toffee/eclair dark pouch.png",
+  },
+  {
+    name: "Eclair Gold Jar",
+    url: "/images/toffee/ECLAIR GOLD JAR.png",
+  },
+  {
+    name: "Eclair Gold",
+    url: "/images/toffee/ECLAIR GOLD.png",
+  },
+  {
+    name: "Eclair Hazelnut Gift Box",
+    url: "/images/toffee/eclair hazelnut gif box.png",
+  },
+  {
+    name: "Eclair Hazelnut Jar",
+    url: "/images/toffee/eclair hazelnut jar.png",
+  },
+  {
+    name: "Eclair Hazelnut Pouch",
+    url: "/images/toffee/eclair hazelnut pouch.png",
+  },
+  {
+    name: "Eclair Hazelnut",
+    url: "/images/toffee/eclair hazelnut.png",
+  },
+  {
+    name: "Eclair Milk Gift Box",
+    url: "/images/toffee/eclair milk gift box.png",
+  },
+  {
+    name: "Eclair Milk Pouch",
+    url: "/images/toffee/eclair milk pouch.png",
+  },
+  {
+    name: "Eclair Milk",
+    url: "/images/toffee/eclair milk.png",
+  },
+  {
+    name: "Eclairs Carmallos",
+    url: "/images/toffee/eclairs carmallos.png",
+  },
+  {
+    name: "Eclairs Milk Jar",
+    url: "/images/toffee/eclairs milk jar.png",
+  },
+  {
+    name: "Eclairs Zupadi Box",
+    url: "/images/toffee/ECLAIRS ZUPADI BOX.png",
+  },
+  {
+    name: "Goa Almond",
+    url: "/images/toffee/GOA ALMOND.png",
+  },
+  {
+    name: "Goa Kaju",
+    url: "/images/toffee/GOA KAJU.png",
+  },
+  {
+    name: "Jewels Zupadi Box",
+    url: "/images/toffee/JEWELS ZUPADI BOX.png",
+  },
+  {
+    name: "Kaju Butter Jar",
+    url: "/images/toffee/KAJU BUTTER JAR.png",
+  },
+  {
+    name: "Kaju Butter Zupadi Box",
+    url: "/images/toffee/KAJU BUTTER ZUPADI BOX.png",
+  },
+  {
+    name: "Kasata Creme Zupadi Box",
+    url: "/images/toffee/KASATA CREME  ZUPADI BOX.png",
+  },
+  {
+    name: "Kesar Cream Zupadi Box",
+    url: "/images/toffee/KESAR CREAM ZUPADI BOX.png",
+  },
+  {
+    name: "Malai Kulfi",
+    url: "/images/toffee/malai kulfi.png",
+  },
+  {
+    name: "Natural Jar",
+    url: "/images/toffee/NATURAL JAR.png",
+  },
+  {
+    name: "Natural Zupadi Box",
+    url: "/images/toffee/NATURAL ZUPADI BOX.png",
+  },
+  {
+    name: "Pan Bites Zupadi Box",
+    url: "/images/toffee/PAN BITES ZUPADI BOX.png",
+  },
+  {
+    name: "Pista Butter Box",
+    url: "/images/toffee/PISTA BUTTER BOX.png",
+  },
+  {
+    name: "Pista Butter Jar",
+    url: "/images/toffee/PISTA BUTTER JAR.png",
+  },
+  {
+    name: "Pista Harvest",
+    url: "/images/toffee/pista harvest.png",
+  },
+  {
+    name: "Soffiti",
+    url: "/images/toffee/soffiti.png",
+  },
+  {
+    name: "Swiss Dairy Zupadi Box",
+    url: "/images/toffee/SWISS DAIRY ZUPADI BOX.png",
+  },
+  {
+    name: "Trasure",
+    url: "/images/toffee/Trasure.png",
+  }],
+  jelly: [ {
+      name: "JELLOS JELLY JAR",
+      url: "/images/jelly/JELLOS JELLY JAR.png",
+    },
+   {
+      name: "JELLOS JELLY",
+      url: "/images/jelly/JELLOS JELLY.png",
+    },
+   {
+      name: "STRAWBERRY JELLY JAR",
+      url: "/images/jelly/STRAWBERRY JELLY JAR.png",
+    },
+   {
+      name: "STRAWBERRY JELLY",
+      url: "/images/jelly/STRAWBERRY JELLY.png",
+    },
+  ],
+  candy: [ {
+      name: "Breating Heart",
+      url: "/images/candy/BEATING HEART.png",
+    },
+   {
+    name: "Double Berry",
+    url: "/images/candy/DOUBLE BERRY.png",
+  },
+{
+    name: "Double Coconut",
+    url: "/images/candy/DOUBLE COCONUT.png",
+  },
+  {
+    name: "Double Guava",
+    url: "/images/candy/DOUBLE GUVAVA.png",
+  },
+  {
+    name: "Double Mango",
+    url: "/images/candy/DOUBLE MANGO.png",
+  },
+  {
+    name: "Double Orange",
+    url: "/images/candy/DOUBLE ORANGE.png",
+  },
+  {
+    name: "Fruit Bon Bon",
+    url: "/images/candy/FRUIT BON BON.png",
+  },
+  {
+    name: "Fruits Candy",
+    url: "/images/candy/FRUITS CANDY.png",
+  },
+  {
+    name: "Imli Candy",
+    url: "/images/candy/IMLI CANDY.png",
+  },
+  {
+    name: "Juzy Candy",
+    url: "/images/candy/JUZY CANDY.png",
+  },
+  {
+    name: "Kacha Aam Candy",
+    url: "/images/candy/KACHA AAM CANDY.png",
+  },
+  {
+    name: "Kacha Aam",
+    url: "/images/candy/KACHA AAM.png",
+  },
+  {
+    name: "Kisszone Dark",
+    url: "/images/candy/KISSZONE DARK.png",
+  },
+  {
+    name: "Kisszone",
+    url: "/images/candy/KISSZONE.png",
+  },
+  {
+    name: "Kisszone01",
+    url: "/images/candy/KISSZONE01.png",
+  },
+  {
+    name: "Kisszone02",
+    url: "/images/candy/KISSZONE02.png",
+  },
+  {
+    name: "Mango Candy",
+    url: "/images/candy/MANGO CANDY.png",
+  },
+  {
+    name: "Ornge Candy",
+    url: "/images/candy/ORNGE CANDY.png",
+  },
+  {
+    name: "Pan Meetha",
+    url: "/images/candy/PAN MEETHA.png",
+  },
+  {
+    name: "Pineapple Candy",
+    url: "/images/candy/PINEAPPLE CANDY.png",
+  },
+  {
+    name: "Strawberry Candy",
+    url: "/images/candy/STRAWBERRY CANDY.png",
+  },
+  {
+    name: "Tambley",
+    url: "/images/candy/TAMBLEY.png",
+  },],
+
+  lollipop: [{
+    name: "Fru Go Pop",
+    url: "/images/lollipop/Fru Go Pop.png",
+  },
+  {
+    name: "Frudoza",
+    url: "/images/lollipop/Frudoza.png",
+  },
+  {
+    name: "Frugo Pop Jar",
+    url: "/images/lollipop/Frugo Pop Jar.png",
+  },
+  {
+    name: "Frugo Pop Pouch",
+    url: "/images/lollipop/Frugo Pop Pouch.png",
+  },
+  {
+    name: "Frutos Lollipop Jar",
+    url: "/images/lollipop/FRUTOS LOLLIPOP JAR.png",
+  },
+  {
+    name: "Lolly Lollipop Pouch",
+    url: "/images/lollipop/LOLLY LOLLIPOP  POUCH.png",
+  },
+  {
+    name: "Lolly Lollipop",
+    url: "/images/lollipop/LOLLY LOLLIPOP.png",
+  }],
 };
 
 const Products = () => {
